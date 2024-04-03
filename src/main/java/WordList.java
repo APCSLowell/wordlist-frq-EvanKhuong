dc66a7fb59931e5ed70970ba5253d9adf5c96d0f
EvanKhuong <119890669+EvanKhuong@users.noreply.github.com>
2024-04-03 12:05:58 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,20 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
+    int n = 0;
+    for (int i = 0; i<myList.size(); i++){
+      if (myList.get(i).length()==len)
+        n++
+    }
     
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
+    for (int i = myList.size(); i>0; i--){
+      if (myList.get(i).length()==len)
+        myList.remove(i);
+    }
 
   }
 }
